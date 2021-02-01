2f4dfa1c46a62997fe52666a049cf7e446e94cc6
jwedter <wedter.jerome@gmail.com>
2021-01-31 23:04:16 -0500

    TP05

---
+++ app/src/main/java/com/google/jerome/wedter/cours_android_jerome_wedter/MainActivity.java
@@ -0,0 +1,14 @@
+package com.google.jerome.wedter.cours_android_jerome_wedter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
